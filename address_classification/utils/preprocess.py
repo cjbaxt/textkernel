03f1762ca5aeed89777f5e2b03b184a17fd05ae8
claire.baxter <claire.baxter@sygno.com>
2022-07-04 21:48:31 +0100

    accessors, preprocessing and project structure

---
+++ address_classification/utils/preprocess.py
@@ -0,0 +1,32 @@
+"""
+Author: Claire Baxter
+Date: 04/07/2021
+Description: Module containing functionality preprocessing the data
+
+Possible future expansions:
+- Fill missing information somehow
+"""
+
+import pandas as pd
+import string
+
+
+def clean_strings(df: pd.DataFrame, col: str):
+    """
+    Function to remove punctuation from strings and convert to lowercase
+
+    Args:
+        df (pandas.DataFrame): Dataframe containing col that we want to clean
+        col (str): Name of the column that we want to clean
+
+    Returns:
+        df (pandas.DataFrame): Cleaned dataframe
+    """
+
+    # Convert to lowercase
+    df[col] = df[col].str.lower()
+
+    # Remove punctuation
+    df[col] = df[col].str.replace('[{}]'.format(string.punctuation), '', regex=True)
+
+    return df
